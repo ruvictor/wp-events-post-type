6ec3038ea2e210f38ffeaa984cc18af12438d4e0
Victor <rusuvictor30@gmail.com>
2021-03-20 01:07:52 -0400

    Custom field assigned

--- events.php
+++ events.php
@@ -51,3 +51,38 @@ function event_post_type() {
         'can_export' => true
     ));
 }
+
+
+// add event date field to events post type
+function add_post_meta_boxes() {
+    add_meta_box(
+        "post_metadata_events_post", // div id containing rendered fields
+        "Event Date", // section heading displayed as text
+        "post_meta_box_events_post", // callback function to render fields
+        "event", // name of post type on which to render fields
+        "side", // location on the screen
+        "low" // placement priority
+    );
+}
+add_action( "admin_init", "add_post_meta_boxes" );
+
+// save field value
+function save_post_meta_boxes(){
+    global $post;
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+    if ( get_post_status( $post->ID ) === 'auto-draft' ) {
+        return;
+    }
+    update_post_meta( $post->ID, "_event_date", sanitize_text_field( $_POST[ "_event_date" ] ) );
+}
+add_action( 'save_post', 'save_post_meta_boxes' );
+
+// callback function to render fields
+function post_meta_box_events_post(){
+    global $post;
+    $custom = get_post_custom( $post->ID );
+    $advertisingCategory = $custom[ "_event_date" ][ 0 ];
+    echo "<input class=\"hasDatepicker\" type=\"date\" name=\"_event_date\" value=\"".$advertisingCategory."\" placeholder=\"Event Date\">";
+}
