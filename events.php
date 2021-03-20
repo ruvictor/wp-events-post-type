a1f04322b0b44c679032e8af739c937476bee208
Victor <rusuvictor30@gmail.com>
2021-03-20 01:45:10 -0400

    Displaying Events

--- events.php
+++ events.php
@@ -17,29 +17,29 @@ add_action('init', 'event_post_type');
 function event_post_type() {
     register_post_type('event', array(
         'labels' => array(
-            'name' => __('Events', 'saltcave'),
-            'singular_name' => __('Event', 'saltcave'),
-            'add_new' => __('Add New', 'saltcave'),
-            'add_new_item' => __('Add New Event', 'saltcave'),
-            'edit_item' => __('Edit Event', 'saltcave'),
-            'new_item' => __('New Event', 'saltcave'),
-            'view_item' => __('View Event', 'saltcave'),
-            'view_items' => __('View Events', 'saltcave'),
-            'search_items' => __('Search Events', 'saltcave'),
-            'not_found' => __('No events found.', 'saltcave'),
-            'not_found_in_trash' => __('No events found in trash.', 'saltcave'),
-            'all_items' => __('All Events', 'saltcave'),
-            'archives' => __('Event Archives', 'saltcave'),
-            'insert_into_item' => __('Insert into service', 'saltcave'),
-            'uploaded_to_this_item' => __('Uploaded to this service', 'saltcave'),
-            'filter_items_list' => __('Filter services list', 'saltcave'),
-            'items_list_navigation' => __('Services list navigation', 'saltcave'),
-            'items_list' => __('Services list', 'saltcave'),
-            'item_published' => __('Service published.', 'saltcave'),
-            'item_published_privately' => __('Service published privately.', 'saltcave'),
-            'item_reverted_to_draft' => __('Service reverted to draft.', 'saltcave'),
-            'item_scheduled' => __('Service scheduled.', 'saltcave'),
-            'item_updated' => __('Service updated.', 'saltcave')
+            'name' => __('Events', 'vicodemedia'),
+            'singular_name' => __('Event', 'vicodemedia'),
+            'add_new' => __('Add New', 'vicodemedia'),
+            'add_new_item' => __('Add New Event', 'vicodemedia'),
+            'edit_item' => __('Edit Event', 'vicodemedia'),
+            'new_item' => __('New Event', 'vicodemedia'),
+            'view_item' => __('View Event', 'vicodemedia'),
+            'view_items' => __('View Events', 'vicodemedia'),
+            'search_items' => __('Search Events', 'vicodemedia'),
+            'not_found' => __('No events found.', 'vicodemedia'),
+            'not_found_in_trash' => __('No events found in trash.', 'vicodemedia'),
+            'all_items' => __('All Events', 'vicodemedia'),
+            'archives' => __('Event Archives', 'vicodemedia'),
+            'insert_into_item' => __('Insert into Event', 'vicodemedia'),
+            'uploaded_to_this_item' => __('Uploaded to this Event', 'vicodemedia'),
+            'filter_items_list' => __('Filter Events list', 'vicodemedia'),
+            'items_list_navigation' => __('Events list navigation', 'vicodemedia'),
+            'items_list' => __('Events list', 'vicodemedia'),
+            'item_published' => __('Event published.', 'vicodemedia'),
+            'item_published_privately' => __('Event published privately.', 'vicodemedia'),
+            'item_reverted_to_draft' => __('Event reverted to draft.', 'vicodemedia'),
+            'item_scheduled' => __('Event scheduled.', 'vicodemedia'),
+            'item_updated' => __('Event updated.', 'vicodemedia')
         ),
         'has_archive'   => true,
         'public' => true,
@@ -72,9 +72,9 @@ function save_post_meta_boxes(){
     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
         return;
     }
-    if ( get_post_status( $post->ID ) === 'auto-draft' ) {
-        return;
-    }
+    // if ( get_post_status( $post->ID ) === 'auto-draft' ) {
+    //     return;
+    // }
     update_post_meta( $post->ID, "_event_date", sanitize_text_field( $_POST[ "_event_date" ] ) );
 }
 add_action( 'save_post', 'save_post_meta_boxes' );
@@ -84,5 +84,34 @@ function post_meta_box_events_post(){
     global $post;
     $custom = get_post_custom( $post->ID );
     $advertisingCategory = $custom[ "_event_date" ][ 0 ];
-    echo "<input class=\"hasDatepicker\" type=\"date\" name=\"_event_date\" value=\"".$advertisingCategory."\" placeholder=\"Event Date\">";
+    echo "<input type=\"date\" name=\"_event_date\" value=\"".$advertisingCategory."\" placeholder=\"Event Date\">";
+}
+
+
+// generate shortcode
+add_shortcode('events-list', 'vm_events');
+function vm_events(){
+    global $post;
+    $args = array(
+        'post_type'=>'event', 
+        'post_status'=>'publish', 
+        'posts_per_page'=>50, 
+        'orderby'=>'meta_value',
+        'meta_key' => '_event_date',
+        'order'=>'ASC'
+    );
+    $query = new WP_Query($args);
+
+    $content = '<ul>';
+    if($query->have_posts()):
+		while($query->have_posts()): $query->the_post();
+            // display event
+            $content .= '<li><a href="'.get_the_permalink().'">'. get_the_title() .'</a> - '.date_format(date_create(get_post_meta($post->ID, '_event_date', true)), 'jS F').'</li>'; 
+        endwhile;
+    else: 
+        _e('Sorry, nothing to display.', 'vicodemedia');
+    endif;
+    $content .= '</ul>';
+
+    return $content;
 }
